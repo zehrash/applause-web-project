d8cc160441e5b2711c8449631cd8360af34eb43a
zehrash <zehrash@users.noreply.github.com>
2021-06-07 16:12:49 +0300

    Fixed database bug

--- server/DAL/applause_gen.sql
+++ server/DAL/applause_gen.sql
@@ -1,33 +1,8 @@
--- phpMyAdmin SQL Dump
--- version 5.1.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: May 23, 2021 at 10:02 PM
--- Server version: 10.4.19-MariaDB
--- PHP Version: 8.0.6
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `applause_gen`
---
---CREATE DATABASE IF NOT EXISTS `applause_gen` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `applause_gen`;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `events`
---
 
 CREATE TABLE IF NOT EXISTS `events` (
   `eventId` int(11) NOT NULL AUTO_INCREMENT,
@@ -36,23 +11,11 @@ CREATE TABLE IF NOT EXISTS `events` (
   PRIMARY KEY (`eventId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `groups`
---
-
 CREATE TABLE IF NOT EXISTS `groups` (
   `groupId` int(11) NOT NULL AUTO_INCREMENT,
   `groupName` varchar(50) NOT NULL,
   PRIMARY KEY (`groupId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `userevents`
---
 
 CREATE TABLE IF NOT EXISTS `userevents` (
   `eventId` int(11) NOT NULL,
@@ -60,23 +23,12 @@ CREATE TABLE IF NOT EXISTS `userevents` (
   PRIMARY KEY (`eventId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `usergroups`
---
 
 CREATE TABLE IF NOT EXISTS `usergroups` (
   `userId` int(11) NOT NULL,
   `groupId` int(11) NOT NULL,
   PRIMARY KEY (`userId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
 
 CREATE TABLE IF NOT EXISTS `users` (
   `userId` int(11) NOT NULL AUTO_INCREMENT,
@@ -85,12 +37,8 @@ CREATE TABLE IF NOT EXISTS `users` (
   `gender` varchar(1) NOT NULL,
   `role` varchar(50) NOT NULL,
   `rating` int(11) NOT NULL,
-  `password` varchar(100) NOT NULL
+  `password` varchar(100) NOT NULL,
   PRIMARY KEY (`userId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- server/DAL/createDb.php
+++ server/DAL/createDb.php
@@ -2,7 +2,7 @@
 function createDb()
 {
 
-    $mysql_host = "localhost";
+    $mysql_host = "127.0.0.1";
     $mysql_database = "applause_gen";
     $mysql_user = "root";
     $mysql_password = "";
@@ -11,8 +11,6 @@ function createDb()
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $create_query = "CREATE DATABASE IF NOT EXISTS `applause_gen` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci";
     $db->exec($create_query);
-
-   // echo "Database created successfully with the name $mysql_database";
 
     $query = file_get_contents("./applause_gen.sql", FILE_USE_INCLUDE_PATH);
 
@@ -23,5 +21,4 @@ function createDb()
     else
         echo "Fail";
 }
-
 createDb();
