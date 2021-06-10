f83a9105e9b71550f3d564b783e1daeba7dc9528
Lyudmila <lusito.pav@gmail.com>
2021-06-10 23:01:55 +0300

    fix form positioning

--- client/helpers/beforeLogin.js
+++ client/helpers/beforeLogin.js
@@ -1,21 +1,17 @@
 function chooseEntranceType() {
     document.getElementById('redirect-to-login').addEventListener('click', (event) => {
-        document.getElementById('login-form').style.visibility='visible';
-        document.getElementById('register-link').style.visibility='visible';
-        document.getElementById('login-link').style.visibility='hidden';
-        document.getElementById('register-form').style.visibility='hidden';
-
-        document.getElementById('login-form').style.textAlign='center';
-        document.getElementById('register-form').style.display = 'none'; 
+        event.preventDefault();
+        document.getElementById('login-form').style.display = 'block';
+        document.getElementById('register-link').style.display = 'block';
+        document.getElementById('login-link').style.display = 'none';
+        document.getElementById('register-form').style.display = 'none';
     });
 
     document.getElementById('redirect-to-register').addEventListener('click', (event) => {
         event.preventDefault();
-        document.getElementById('register-form').style.visibility = 'visible';
-        document.getElementById('register-form').style.display='contents';
-
-        document.getElementById('login-form').style.visibility = 'hidden';
-        document.getElementById('register-link').style.visibility = 'hidden';
-        document.getElementById('login-link').style.visibility = 'visible';
+        document.getElementById('login-form').style.display = 'none';
+        document.getElementById('register-link').style.display = 'none';
+        document.getElementById('login-link').style.display = 'block';
+        document.getElementById('register-form').style.display = 'block';
     });
 }
