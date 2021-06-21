f3969c758acc9808ffd61f91734858e4bd23a278
zehrash <zehrash@users.noreply.github.com>
2021-06-21 12:27:13 +0300

    Fixed seat saving to db and seat ui updating

--- server/saveseat.php
+++ server/saveseat.php
@@ -1,14 +1,23 @@
 <?php
 
 set_include_path(getcwd()); 
-include './DAL/queries.php';
+require_once( './DAL/queries.php');
 session_start();
 
 if ($_SERVER['REQUEST_METHOD'] === "POST" && isset($_POST["seatId"])) {
-    $savedseatId = saveSeat($_SESSION["eventId"], 1, $_POST["seatId"]);
+ 
+    $savedseatId = saveSeat($_SESSION["eventId"], $_SESSION["userId"], $_POST["seatId"]);
 
-    $_SESSION['seatId'] = $savedseatId;
-   echo json_encode(["message" => "seat save successfully"]);
+   $_SESSION['seatId'] = $savedseatId;
+  // echo json_encode(["message" => "seat save successfully"]);
+
+   $response = json_encode([
+    "message" => "seat save successfully", 
+    "seat" => $_POST["seatId"],
+    "userid " => $_SESSION["userId"],
+    "eventid" =>$_SESSION["eventId"]
+]);
+echo $response;
 } else{
     echo json_encode(["message" =>'tried to save seat but it didnt work']);
 }
